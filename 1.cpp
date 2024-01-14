f9e8110210b18855431654e4255ea6f0f0c40e10
Ayush <ayush.krsh9@gmail.com>
2024-01-14 10:32:52 +0530

    re

--- 1.cpp
+++ 1.cpp
@@ -3,8 +3,7 @@
 int main()
 {
     int n = 5;
-    for (int i = 1; i <= n; ++i)
-    {
+    for (int i = 1; i <= n; ++i){
         for (int j = 1; j <= n - i; ++j)
         {
             std::cout << " ";
