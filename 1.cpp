816eb8830cdc4f6271cb1b7dadf03dbfec35dba5
Ayush <ayush.krsh9@gmail.com>
2024-01-16 20:58:02 +0530

    12

--- 1.cpp
+++ 1.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    int n = 5;
-    for (int i = 1; i <= n; ++i){
-        for (int j = 1; j <= n - i; ++j)
-        {
-            std::cout << " ";
-        }
-        for (int k = 1; k <= 2 * i - 1; ++k)
-        {
-            std::cout << "*";
-        }
-
-        // Move to the next line after each row
-        std::cout << std::endl;
-    }
-    return 0;
+   cout<<"Hello Probot"<<endl;
+   return 0;
+    
 }
